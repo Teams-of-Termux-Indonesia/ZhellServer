40651e31bb2575207f5fad4109df3fda8744d156
京 Ryugen <109148993+ryugenxd@users.noreply.github.com>
2022-09-21 23:28:57 +0800

    Update configmysql.sh

--- bin/configmysql.sh
+++ bin/configmysql.sh
@@ -1,3 +1,3 @@
-printf "\033[32m SILAHKAN MASUKKAN PERINTAH SQL DEFAULT \033[0m";
+printf "\033[32m SILAHKAN MASUKKAN PERINTAH SQL DEFAULT \033[0m\n";
+printf "\033[1;33mNew Session Termux nya bro di sessi ke dua masukan perintah: \033[1;31mmysqld_safe\033[0m\n";
 printf "\033[32m ☺️ Yang ada di github Di kolom Deskripsi ! \033[0m";
-mysql -u $(whoami);
