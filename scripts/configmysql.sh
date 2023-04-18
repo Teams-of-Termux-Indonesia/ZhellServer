795ea733dda26f49ba37849d5cb4f48d8541ea8c
ryugenxd <anonimous7770@gmail.com>
2023-04-18 17:14:01 +0800

    update

--- scripts/configmysql.sh
+++ scripts/configmysql.sh
@@ -27,16 +27,16 @@ clear
 printf "\033[1;35m"
 figlet $PACKAGE_NAME
 # Tampilkan pesan sukses dan keterangan login
-printf "\033[33mKonfigurasi Database:\nHOST : 127.0.0.1\nUSERNAME : root\nPASSWORD : root\n"
+printf "\033[33mKonfigurasi Database:\n \033[41;37mHOST\033[0m] : 127.0.0.1\n \033[41;37mUSERNAME\033[0m : root\n \033[41;37mPASSWORD\033[0m] : root\n"
 printf "\033[32mKonfigurasi Berhasil!"
 printf "\n\033[0m-------------------------------"
 printf "\n\033[1;33mURL LOCAL SEVER  : \033[1;34mhttp://localhost:8080\033[1;33m"
-printf "\nHTDOCS PATCH : \033[1;34m/sdcard/htdocs/"
+printf "\nHTDOCS PATCH : \033[1;34mlokasi htdocs tersimpan di memory internal android dengan nama folder: \033[41;37mhtdocs\033[0m"
 printf "\033[1;32m\nSelamat Bro!\n"
 printf "\033[1;33mWARNING : \033[41;37mServer Lokal tidak akan berjalan jika aplikasi termux tidak di reload !\033[0m"
 while [ true ]
 do
-    printf "\n\033[1;31mReload (y): "
+    printf "\n\033[1;31mReload (ketik y lalu enter): "
     read confirm
     if [ "$confirm" == "y" ] && [ "${#confirm}" -eq 1 ]
     then
