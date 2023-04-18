2e0283612a0afc958ac601b508823cf6cf098694
ryugenxd <anonimous7770@gmail.com>
2023-04-18 14:50:38 +0800

    update && fixed issues

--- scripts/configmysql.sh
+++ scripts/configmysql.sh
@@ -1,10 +1,10 @@
 printf "\033[1;33m konfigurasi : S\033[1;31m mysql \033[0m\n";
-mysqld_safe &;
+mysqld_safe &
 mysql -u root <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
 FLUSH PRIVILEGES;
 exit;
-EOF;
+EOF
 printf "\033[33m username: root pass: root]";
 printf "\033[32m konfigurasi success! \033[0m\n";
 printf "\033[32m happy coding ! \033[0m\n";
