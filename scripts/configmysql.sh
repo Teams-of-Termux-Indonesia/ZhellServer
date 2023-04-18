073b50e1c9d7aa96f284b2685026db8c8470afdb
ryugenxd <anonimous7770@gmail.com>
2023-04-18 16:58:09 +0800

    update

--- scripts/configmysql.sh
+++ scripts/configmysql.sh
@@ -33,13 +33,14 @@ printf "\n\033[0m-------------------------------"
 printf "\n\033[1;33mURL LOCAL SEVER  : \033[1;34mhttp://localhost:8080\033[1;33m"
 printf "\nHTDOCS PATCH : /sdcard/htdocs/"
 printf "\033[1;32m\nSelamat Bro!\n"
-printf "WARNING : server tidak akan berjalan jika aplikasi termux tidak di reload !"
+printf "\033[1;33mWARNING : Server Lokal tidak akan berjalan jika aplikasi termux tidak di reload !"
 while [ true ]
 do
-    printf "reload (y): "
+    printf "\nReload (y): "
     read confirm
-    if [ $confirm == 'y' ]
+    if [ "$confirm" == "y" ] && [ "${#confirm}" -eq 1 ]
     then
+        login
         break
     fi
 done
