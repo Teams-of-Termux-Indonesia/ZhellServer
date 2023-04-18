b8c4ebdaba21e6ee778137ad1e4af072b6bc36b1
ryugenxd <anonimous7770@gmail.com>
2023-04-18 17:25:35 +0800

    update

--- scripts/configmysql.sh
+++ scripts/configmysql.sh
@@ -1,6 +1,13 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-PACKAGE_NAME="ZhellServer"
+PACKAGE_NAME="
+███████╗██╗░░██╗███████╗██╗░░░░░██╗░░░░░
+╚════██║██║░░██║██╔════╝██║░░░░░██║░░░░░
+░░███╔═╝███████║█████╗░░██║░░░░░██║░░░░░
+██╔══╝░░██╔══██║██╔══╝░░██║░░░░░██║░░░░░
+███████╗██║░░██║███████╗███████╗███████╗
+╚══════╝╚═╝░░╚═╝╚══════╝╚══════╝╚══════╝
+"
 
 printf "\033[1;33m konfigurasi : MySQL \033[0m\n"
 
@@ -25,9 +32,9 @@ sed -i "1s/^/apachectl start\nmysqld_safe &\n" $PREFIX/etc/bash.bashrc
 # Bersihkan layar konsol
 clear
 printf "\033[1;35m"
-figlet $PACKAGE_NAME
+
 # Tampilkan pesan sukses dan keterangan login
-printf "\033[33mKonfigurasi Database:\n \033[41;37mHOST\033[0m] : 127.0.0.1\n \033[41;37mUSERNAME\033[0m : root\n \033[41;37mPASSWORD\033[0m] : root\n"
+printf "\033[33mKonfigurasi Database:\n \033[41;37mHOST\033[0m : 127.0.0.1\n \033[41;37mUSERNAME\033[0m : root\n \033[41;37mPASSWORD\033[0m : root\n"
 printf "\033[32mKonfigurasi Berhasil!"
 printf "\n\033[0m-------------------------------"
 printf "\n\033[1;33mURL LOCAL SEVER  : \033[1;34mhttp://localhost:8080\033[1;33m"
@@ -36,11 +43,11 @@ printf "\033[1;32m\nSelamat Bro!\n"
 printf "\033[1;33mWARNING : \033[41;37mServer Lokal tidak akan berjalan jika aplikasi termux tidak di reload !\033[0m"
 while [ true ]
 do
-    printf "\n\033[1;31mReload (ketik y lalu enter): "
+    printf "\n\n\033[1;32mReload (\033[1;33mketik y lalu enter\033[0m):\033[0m "
     read confirm
-    if [ "$confirm" == "y" ] && [ "${#confirm}" -eq 1 ]
+    if [ "$confirm" == "y" ]
     then
-        login
+        echo "ok"
         break
     fi
 done
