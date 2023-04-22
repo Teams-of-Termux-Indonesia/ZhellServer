88ad78b3bf5ab20cc472c85b123252f8130e4d45
京 Ryugen <109148993+ryugenxd@users.noreply.github.com>
2023-04-22 11:00:04 +0800

    Update install.sh
    add fiture multi vhost htdocs + phpmyadmin

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,7 +18,7 @@ cp -f config_files/httpd.conf $PREFIX/etc/apache2;
 httpd -t;
 rm -f $PREFIX/share/phpmyadmin/config.inc.php;
 cp -f config_files/config.inc.php $PREFIX/share/phpmyadmin;
-mv $PREFIX/share/phpmyadmin $HOME; #add anu anu
+mv $PREFIX/share/phpmyadmin /sdcard/htdocs; #add anu anu
 printf "\033[1;33m install selesai full size: 700mb \033[1;31m^\033[0m\n";
 printf "\033[1;33m # Setting konfigurasi mysql\033[0m\n";
 sh scripts/configmysql.sh;
