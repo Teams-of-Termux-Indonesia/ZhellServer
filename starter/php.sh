99cff987117656823a707570b707aae0e95e1f15
Ryugen 京 <109148993+ryugenxd@users.noreply.github.com>
2024-03-15 23:12:03 +0800

    Update php.sh

--- starter/php.sh
+++ starter/php.sh
@@ -3,10 +3,10 @@ pkg install php -y;
 pkg install composer -y;
 pkg install mariadb -y;
 mysqld_safe &
+wait
 mysql -u root <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
 FLUSH PRIVILEGES;
 exit;
 EOF
-wait
 echo "\033[1;32mSuccessfully configured mysql !\nmysql user: root \nmysql password: root\033[0m";
