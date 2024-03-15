78b25dea15004a8a453fd16ac64526db4b0f41b2
Ryugen 京 <109148993+ryugenxd@users.noreply.github.com>
2024-03-15 22:43:54 +0800

    Update php.sh

--- starter/php.sh
+++ starter/php.sh
@@ -2,5 +2,9 @@
 pkg install php -y;
 pkg install composer -y;
 pkg install mariadb -y;
-mysqld_safe &;
-echo "hello world";
+mysqld_safe &
+mysql -u root <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
+FLUSH PRIVILEGES;
+exit;
+EOF
